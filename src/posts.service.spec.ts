d0501e34e7b009b1b2582dd7b2ea65b0441a1696
Sofia Purtova <sofiapurtova@yandex.ru>
2025-08-17 11:04:27 +0300

    тесты реализованы

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,3 +1,4 @@
+import exp from 'constants';
 import { Post, PostsService } from './posts.service';
 
 describe('PostsService', () => {
@@ -14,9 +15,24 @@ describe('PostsService', () => {
 
   it('should add a new post', () => {
     // реализуйте тест-кейс
+    const newPost = postsService.create(post);
+    expect(newPost.text).toBe(post.text);
+
+    expect(newPost.id).toBeDefined();
+    expect(typeof newPost.id).toBe('string');
+
+    expect(newPost.date).toBeDefined();
+    expect(() => new Date(newPost.date)).not.toThrow();
+
+    expect(postsService.find(newPost.id)).toEqual(newPost);
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
+    const createdPost = postsService.create(post);
+    const foundPost = postsService.find(createdPost.id);
+
+    expect(foundPost).toEqual(createdPost);
+    expect(postsService.find('undefined-id')).toBeUndefined();
   });
 });
